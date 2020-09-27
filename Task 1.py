82a8b946ad2020c1572c844835ef49b90f1b8c24
Mohammad Nasfikur Rahman Khan <71976646+Nasfikur-Khan@users.noreply.github.com>
2020-09-27 20:57:49 +0600

    Data_Visualization_Task1

---
+++ Task 1.py
@@ -0,0 +1,15 @@
+#Import Pandas and matplotlib
+
+import pandas as pd
+import matplotlibe.pyplot as plt
+
+#Create the dataframesusing the json file
+df = pd.read_json(r'/rain,json')
+print(df)
+
+print('df statistic: ', df.describe())
+
+df.plot(x = 'Month', y = 'Temperature')
+df.plot(x = 'Month', y = 'Rainfall')
+
+plt.show()
