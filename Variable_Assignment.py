530a82a96ed9bb9b4b3280ac6370626b75755799
Mohammad Nasfikur Rahman Khan <71976646+Nasfikur-Khan@users.noreply.github.com>
2020-09-27 21:01:18 +0600

    Basic_Variable_Assignment

---
+++ Variable_Assignment.py
@@ -0,0 +1,9 @@
+#My Favorite Song:
+
+singer = "Tahsan Khan"
+genre = 'Soft Rock'
+Year_of_publish = 2003
+Duration_in_seconds = 330
+Music_Language = 'Bengali'
+
+print("My favorite song are sang by: " + singer + " and it was sang in " + str(Year_of_publish) + ". This song is around " + str(Duration_in_seconds) + " seconds long" + " and from " + str(genre) + " genre, sang in "+ str(Music_Language) + ' language')
